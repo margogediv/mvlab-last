b31d5cf32dbf69f11b2e338115d087384e4069aa
Pavel Zholnerovich <pavel.zholnerovich@yandex.ru>
2020-10-09 16:51:35 +0300

    rm .nuxt and .idea

--- .nuxt/webfontloader.js
+++
@@ -1,6 +0,0 @@
-
-import WebFont from 'webfontloader'
-
-const options = {"events":false,"google":{"families":["Montserrat:400,500,600:cyrillic&display=swap"]},"timeout":5000}
-
-WebFont.load(options)
